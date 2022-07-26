1dc743c1e9d302b5316e43d293aff6521632e53d
vidhidave <vidhi@e2logy.com>
2022-07-26 11:59:36 +0530

    Added feature 2

--- Greeter.java
+++ Greeter.java
@@ -14,7 +14,7 @@ public class Greeter {
 
   public String sayHello() {
     ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
-    return messages.getString("greeting") + ", " + name;
+    return messages.getString("greeting") + ", " + ” ”+name+” ”;
   }
 
   public static void main(String[] args) {
